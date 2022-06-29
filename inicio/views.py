efdd176270697850196feeff5d0e6022bc98910c
Charliedev-11095 <carlosroyiga@gmail.com>
2022-06-28 20:50:41 -0500

    tabla visualizada

--- inicio/views.py
+++ inicio/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render, HttpResponse
 
 # Create your views here.
-def principal(request):
-    return render(request,"inicio/principal.html")
 
 def contacto(request):
     return render(request,"inicio/contacto.html")
--- prueba/urls.py
+++ prueba/urls.py
@@ -17,10 +17,11 @@ from django.contrib import admin
 from django.urls import path
 from inicio import views
 from django.conf import settings
+from registros import views as views_registros
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.principal, name="Principal"),
+    path('',views_registros.registros, name="Principal"),
     path('contacto/', views.contacto, name="Contacto"),
     path('formulario/', views.formulario, name="Formulario"),
     path('ejemplo/', views.ejemplo, name="Ejemplo"),
