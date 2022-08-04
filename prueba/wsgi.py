0191f23ee9790e5b7a96741435ed96814d394a1f
CharlX <68936309+Charliedev-11095@users.noreply.github.com>
2022-08-04 08:19:02 -0500

    update wsgi.py
    04/08/22

--- prueba/wsgi.py
+++ prueba/wsgi.py
@@ -8,9 +8,14 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
-from django.core.wsgi import get_wsgi_application
+path = os.path.expanduser('~/Charliedev-11095/prueba')
+if path not in sys.path:
+    sys.path.insert(0,path)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'prueba.settings')
+from django.core.wsgi import get_wsgi_application
+from django.contrib.staticfiles.handlers import StaticFilesHandler
 
-application = get_wsgi_application()
+application = StaticFilesHandler(get_wsgi_application())
